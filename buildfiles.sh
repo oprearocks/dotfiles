6e103c036bcd9d159cbff9c4bf8300bbee0fe534
Adrian Oprea <opreaadriangeorge@gmail.com>
2013-12-17 14:15:45 +0200

    Updates

--- buildfiles.sh
+++ buildfiles.sh
@@ -7,8 +7,8 @@ sudo -v
 for file in ".ackrc" ".aliases" ".bash_profile" ".bash_prompt" ".bashrc" \
         ".exports" ".functions" ".gitattributes" ".gitconfig" ".gitignore" \
             ".inputrc" ".vimrc" ".tmux.conf" ".zshrc" ".vim"; do
-    mv "$HOME/$file" "$HOME/dotfiles/" > /dev/null 2>&1
-# Symlink the files back to ~/
+    # mv "$HOME/$file" "$HOME/dotfiles/" > /dev/null 2>&1
+    # Symlink the files back to ~/
     ln -s "$HOME/dotfiles/$file" "$HOME/$file" > /dev/null 2>&1
 done
 echo "Done. For the changes to take effect, open a new tab or restart your terminal app."
