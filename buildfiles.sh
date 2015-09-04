3799f86c2cd1be8420d6360a38329f7d50bac9e1
Adrian Oprea <opreaadriangeorge@gmail.com>
2015-09-04 23:06:54 +0300

    fix(buildfiles): Update symlink generating script to use unprefixed filenames

--- buildfiles.sh
+++ buildfiles.sh
@@ -4,12 +4,12 @@
 sudo -v
 
 # Get all files and move them to our dotfiles directory
-for file in ".ackrc" ".aliases" ".bash_profile" ".bash_prompt" ".bashrc" \
-        ".exports" ".functions" ".gitattributes" ".gitconfig" ".gitignore" \
-            ".inputrc" ".vimrc" ".tmux.conf" ".zshrc" ".vim" ".curlrc" \
-            ".wgetrc" ; do
+for file in "ackrc" "aliases" "bash_profile" "bash_prompt" "bashrc" \
+        "exports" "functions" "gitconfig" \
+            "inputrc" "vimrc" "tmux.conf" "zshrc" "vim" "curlrc" \
+            "wgetrc" ; do
     # mv "$HOME/$file" "$HOME/dotfiles/" > /dev/null 2>&1
     # Symlink the files back to ~/
-    ln -s "$HOME/dotfiles/$file" "$HOME/$file" > /dev/null 2>&1
+    ln -s "$HOME/dotfiles/$file" "$HOME/.$file" > /dev/null 2>&1
 done
 echo "Done. For the changes to take effect, open a new tab or restart your terminal app."
