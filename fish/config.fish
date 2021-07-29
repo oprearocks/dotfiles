cc843777cc74204640eb6cadacce9aeb06a7af10
Adrian Oprea <adrian@oprea.rocks>
2021-07-30 02:05:32 +0300

    Remove thefuck altogether

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,10 @@
 source ~/.config/fish/functions/z.fish
-set PATH /opt/homebrew/local/bin $PATH
-
-thefuck --alias | source 
 
 set -x EDITOR nvim
 set -x GOPATH "$HOME/go-projects"
 rvm default
 rvm default
+
+set PATH /opt/homebrew/bin $PATH
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
